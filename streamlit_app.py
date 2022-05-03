fddc6bd6e0fdf831166e7383d629bc74a56e15b5
smurali97snowflake <104405966+smurali97snowflake@users.noreply.github.com>
2022-05-02 23:49:43 -0400

    Update streamlit_app.py
    changing URL

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,8 +25,9 @@ streamlit.dataframe(my_fruit_list)
 
 streamlit.header('Fruityvice Fruit Advice!')
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+
+#streamlit.text(fruityvice_response.json())
 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 
