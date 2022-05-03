8ebf4a3a2bc768ae3e36e9e536ea5ddd7533edde
smurali97snowflake <104405966+smurali97snowflake@users.noreply.github.com>
2022-05-02 23:52:54 -0400

    Update streamlit_app.py
    taking fruit choice from user

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,19 +17,19 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 my_fruit_list = my_fruit_list.set_index('Fruit')
 #streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
-
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 streamlit.dataframe(my_fruit_list)
 
 streamlit.header('Fruityvice Fruit Advice!')
+fruit_choice = steamlit.text_input('What fruit would you like information about?','kiwi')
+streamlit.write('The User entered', fruit_choice)
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 
+#fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+#fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 #streamlit.text(fruityvice_response.json())
 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-
-
 streamlit.dataframe(fruityvice_normalized)
